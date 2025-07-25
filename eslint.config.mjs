e89438db8140b9265f865bc7648a30f58baa8eec
侑介 <spark9487111@gmail.com>
2025-07-25 18:41:29 +0800

    chore: update lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,13 +2,25 @@ import eslint from "@eslint/js";
 import vue from "eslint-plugin-vue";
 import vueParser from "vue-eslint-parser";
 import prettierConfig from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
 
 export default [
   {
     ignores: ["node_modules", "dist", "build"],
   },
   eslint.configs.recommended,
+  ...tseslint.configs.recommended,
   prettierConfig,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+  },
   {
     files: ["**/*.vue"],
     languageOptions: {
@@ -16,6 +28,7 @@ export default [
       parserOptions: {
         ecmaVersion: "latest",
         sourceType: "module",
+        parser: tseslint.parser,
         extraFileExtensions: [".vue"],
       },
     },
