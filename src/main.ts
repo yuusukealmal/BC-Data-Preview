31a2c7966c8246247be57d3774c925008d6380b5
侑介 <spark9487111@gmail.com>
2025-08-03 18:46:11 +0800

    feat: add router

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+
+import { router } from "./router";
+
 import "./style.css";
+
 import App from "./App.vue";
+import { applyTheme } from "./utils/theme";
+
+const isDarked = localStorage.getItem("isDark");
+if (isDarked) {
+  const theme = JSON.parse(isDarked);
+  applyTheme(theme);
+} else {
+  applyTheme(false);
+}
 
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(pinia);
+app.use(router);
+
 app.mount("#app");
